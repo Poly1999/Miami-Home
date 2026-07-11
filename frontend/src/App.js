67ac25f71a8337998c0ea7885dbeec40a1b30f91
Poly1999 <pikalova.developer@gmail.com>
2026-07-11 12:15:57 -0400

    set up react components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import Gallery from './components/Gallery/Gallery';
+import Testimonials from './components/Testimonials/Testimonials';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Header />
+      <Hero />
+      <About />
+      <Gallery />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
