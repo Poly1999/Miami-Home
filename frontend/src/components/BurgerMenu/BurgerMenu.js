526dcbecc29e27583515f9caff4de90f7b069439
Poly1999 <pikalova.developer@gmail.com>
2026-07-14 20:57:43 -0400

    add BurgerMenu component with open/close logic

--- frontend/src/components/BurgerMenu/BurgerMenu.js
+++ frontend/src/components/BurgerMenu/BurgerMenu.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import './BurgerMenu.css';
 
-function BurgerMenu() {
-  return <div>BurgerMenu</div>;
+import { ReactComponent as Close } from '../../assets/burgerClose.svg';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+
+function BurgerMenu({ onClose }) {
+  return (
+    <div className='burger-container'>
+      <div className='container'>
+        <div className='burger-header'>
+          <Close onClick={onClose} />
+          <Logo />
+        </div>
+
+        <div className='burger-box'>
+          <ul className='burger-list'>
+            <li className='burger-item'>Home</li>
+            <li className='burger-item'>About Us</li>
+            <li className='burger-item'>Gallery</li>
+            <li className='burger-item'>Testimonials</li>
+            <li className='burger-item'>Contacts</li>
+          </ul>
+          <div className='burger-contact'>
+            <p className='burger-phone'>+1 234 555-55-55</p>
+            <button className='burger-buttton'>Call to order</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BurgerMenu;
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 import { ReactComponent as Burger } from '../../assets/burger.svg';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 import { ReactComponent as Phone } from '../../assets/phone.svg';
+import BurgerMenu from '../BurgerMenu/BurgerMenu';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header>
       <div className='container'>
         <div className='header-container'>
-          <Burger />
+          <Burger onClick={() => setIsOpen(true)} />
           <Logo />
           <Phone />
         </div>
       </div>
+      {isOpen && <BurgerMenu />}
+      {isOpen && <BurgerMenu onClose={() => setIsOpen(false)} />}
     </header>
   );
 }
